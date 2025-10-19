7d6d264cc2967eb2105bb3234ca09b795fbe0064
Briony Frater <briefrater@gmail.com>
2025-10-19 08:55:47 -0500

    fixed header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Header() {
     return (
         <header className="fixed top-0 w-full bg-background gap-4 py-4 px-6 flex items-center justify-start">
-            <Image alt="" src={"/logo_alt.png"} width={50} height={50}></Image>
+            <Image alt="" src={"/alt_logo.png"} width={50} height={50}></Image>
             <h1 className="font-medium">Hear Me</h1>
         </header>
     );
